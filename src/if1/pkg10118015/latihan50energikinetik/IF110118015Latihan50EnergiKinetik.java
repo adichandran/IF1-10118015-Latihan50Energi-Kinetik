af254e9d9279802225cfb4783edc7f66f4518067
adichandran <55467774+adichandran@users.noreply.github.com>
2019-10-19 08:31:51 +0700

    Add files via upload

---
+++ src/if1/pkg10118015/latihan50energikinetik/IF110118015Latihan50EnergiKinetik.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118015.latihan50energikinetik;
+
+/**
+ *
+ * @author ACER
+ */
+public class IF110118015Latihan50EnergiKinetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EnergiKinetik ek = new EnergiKinetik();
+        
+        ek.setMassa(145);
+        ek.setKecepatan(25);
+        
+        System.out.println("===Program Menghitung Energi Kinetik===");
+        System.out.println("Massa benda = "+ ek.getMassa() + " gram");
+        System.out.println("Kecepatan benda = "+ ek.getKecepatan() + " m/s");
+        System.out.println("");
+        System.out.println("Energi kinetiknya adalah " + ek.energiKinetik(ek.getMassa(), ek.getKecepatan()));
+        System.out.println("Usaha yang dilakukan dalam keadaan diam adalah "
+        + ek.ekDiam(ek.energiKinetik(ek.getMassa(), ek.getKecepatan())));
+    }
+    
+}
